f29cc8a92f41f2e4bf010b56122e28424d932c1e
Isma'il Amin <ismail.amin@ucdenver.edu>
2026-04-02 12:18:00 -0600

    modely.py: added metadata scraping for checking for any new job postings, set currency, alternated the format, set logic for remote locations ; README: changed formatting

--- models.py
+++ models.py
@@ -1,13 +1,28 @@
 from dataclasses import dataclass
-from typing import Optional # for potentially missing values
+from typing import Optional  # for potentially missing values
+from datetime import datetime  # for grabbing metadata for automation
+
 
 @dataclass
 class JobListing:
     """Class that will format raw data"""
+    # core identification
     job_id: str
     company: str
     title: str
-    location: str
-    department: Optional[str] = None
+
+    # location data
+    location_raw: str
+    is_remote: bool = False  # for "separates remote locations into their own category"
+
+    # filtering for trends
+    department: str
+
+    # financial data: job postings sometimes don't show the min/max for salary --> Optional
     salary_min: Optional[int] = None
-    salary_max: Optional[int] = None
+    salry_max: Optional[int] = None
+    currency: str = "USD"
+
+    # metadata: checking for any new job postings each week
+    date_scraped: datetime = datetime.now()
+    url: str = ""
